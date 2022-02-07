61e47e3e516f86ea3949b7de0ed448daa07afed7
Przemysław Kołodziej <pkolodziej97@gmail.com>
2022-02-08 00:18:11 +0100

    Today I created a table with english words and describtsions.

---
+++ zExcelaDoBazy/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace zExcelaDoBazy
+{
+    class Program
+    {
+        static string pobranaBaza = @"C:\Users\pkolo\Downloads\englishDictionary.csv";
+        static string baza2 = @"C:\Users\pkolo\Downloads\englishDictionaryObrobiony.csv";
+        static void Main(string[] args)
+        {
+            zExcelaDoExcela();
+            Console.WriteLine("Hello World!");
+        }
+        static void zExcelaDoExcela()
+        {
+            string line;
+            int i;
+            using (StreamWriter pisana = new StreamWriter(baza2)) 
+            using (StreamReader czytana = new StreamReader(pobranaBaza))
+            {
+                while ((line = czytana.ReadLine()) != null)
+                {
+                    i = 0;
+                    for(; i < line.Length && line[i]!=',' && line[i] != ' '; i++)
+                    {
+                        pisana.Write(line[i]);
+                    }
+                    pisana.Write(';');
+                    while(i<line.Length)
+                    {
+                        pisana.Write(line[i++]);
+                    }
+                    pisana.WriteLine();
+                }
+
+            }
+
+        }
+    }
+}
